b35270b95eb51178f7966f8bbc7fee80157b801d
Saim <ssimsek@smail.uni-koeln.de>
2021-08-19 15:46:36 +0200

    Update aero.py

--- aero/aero.py
+++ aero/aero.py
@@ -1,13 +1,13 @@
 from redbot.core import commands
 
-class MyCog(commands.Cog):
+class Aero(commands.Cog):
     """My custom cog"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def aero(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
