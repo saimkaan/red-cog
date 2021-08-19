bc88dc24b3d024ff03e47cf42c422b1a6ceaf588
Saim <ssimsek@smail.uni-koeln.de>
2021-08-19 17:17:03 +0200

    Update aero.py

--- aero/aero.py
+++ aero/aero.py
@@ -15,3 +15,12 @@ class Aero(commands.Cog):
             async with message.channel.typing():
                 await message.reply("Aero says fuck you, and TA is fake news")
             return
+        
+    @commands.Cog.listener()
+    async def on_message_without_command(self, message: discord.Message):
+        if message.author.bot:
+            return
+        if message.content.lower() == "aero":
+            async with message.channel.typing():
+                await message.reply("Aero says fuck you, and TA is fake news")
+            return
