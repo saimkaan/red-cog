61c3af3ce017362a1f5cb278df051057ed77dc41
Saim <ssimsek@smail.uni-koeln.de>
2021-08-19 15:40:53 +0200

    Update aero.py

--- aero/aero.py
+++ aero/aero.py
@@ -1,24 +1,22 @@
-from redbot.core import commands
+import discord
+from discord.ext import commands
 
-class Aero(commands.Cog):
-    """My custom cog for Aero"""
+client = commands.Bot(command_prefix='!')
 
-    def __init__(self, bot):
-        self.bot = bot
-        
-    client = commands.Bot(command_prefix='!')
-    
-    @client.event
-    async def on_ready():
-        print('client ready')
+@client.event
+async def on_ready():
+    print('client ready')
 
-    @client.command()
-    async def ping():
-        await client.say('Pong')
+@client.command()
+async def ping():
+    await client.say('Pong')
 
-    @client.event
-    async def on_message(message):
-        if client.user.id != message.author.id:
-            if 'foo' in message.content:
-                await client.send_message(message.channel, 'bar')
-        await client.process_commands(message)
+@client.event
+async def on_message(message):
+    if client.user.id != message.author.id:
+        if 'foo' in message.content:
+            await client.send_message(message.channel, 'bar')
+
+    await client.process_commands(message)
+
+client.run('TOKEN')
