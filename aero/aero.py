cfb8b7bea6962c97a121829832a9b3511124517f
Saim <ssimsek@smail.uni-koeln.de>
2021-08-19 17:46:53 +0200

    Update aero.py

--- aero/aero.py
+++ aero/aero.py
@@ -11,7 +11,7 @@ class Aero(commands.Cog):
     async def on_message_without_command(self, message: discord.Message):
         if message.author.bot:
             return
-        if "foo" or "Foo" in message.content:
+        if "foo" in message.content:
             async with message.channel.typing():
                 await message.reply("bar")
             return
