36d2a8ce72a58f636d99293f9883caf6ea5c9987
Saim <ssimsek@smail.uni-koeln.de>
2021-08-19 16:37:39 +0200

    Update aero.py

--- aero/aero.py
+++ aero/aero.py
@@ -9,6 +9,5 @@ class Aero(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message):
-        if client.user.id != message.author.id:
-            if 'foo' in message.content:
-                await client.send_message(message.channel, 'bar')
+        if 'foo' in message.content:
+            await client.send_message(message.channel, 'bar')
