129e90316f38d63c95d70915233244f2eec5b797
Saim Simsek <saimkaan.simsek@cbc.de>
2021-08-20 13:43:17 +0200

    aero bot command

--- aero/aero.py
+++ aero/aero.py
@@ -7,22 +7,11 @@ class Aero(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
             
-    # @commands.Cog.listener()
-    # async def on_message_without_command(self, message: discord.Message):
-    #     if message.author.bot:
-    #         return
-    #     if "aero" in message.content.lower():
-    #         async with message.channel.typing():
-    #             await message.reply("Aero says fuck you, and TA is fake news")
-    #         return
-
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message):
-        aeromention = ["aero"]
-        aeroanswer = ["Aero says fuck you, and TA is fake news"]
         if message.author.bot:
             return
-        if aeromention in message.content.lower():
+        if "aero" in message.content.lower():
             async with message.channel.typing():
-                await message.channel.send(aeroanswer)
+                await message.reply("Aero says fuck you, and TA is fake news")
             return
