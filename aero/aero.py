d1924ddc521ded73aaee0b260a6984543dcb3e45
Saim Simsek <saimkaan.simsek@cbc.de>
2021-08-20 13:38:41 +0200

    aero cleanup code

--- aero/aero.py
+++ aero/aero.py
@@ -7,11 +7,22 @@ class Aero(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
             
-    @commands.Cog.listener()
+    # @commands.Cog.listener("Working")
+    # async def on_message_without_command(self, message: discord.Message):
+    #     if message.author.bot:
+    #         return
+    #     if "aero" in message.content.lower():
+    #         async with message.channel.typing():
+    #             await message.reply("Aero says fuck you, and TA is fake news")
+    #         return
+
+    @commands.Cog.listener("Testing")
     async def on_message_without_command(self, message: discord.Message):
+        aeromention = ["aero"]
+        aeroanswer = ["Aero says fuck you, and TA is fake news"]
         if message.author.bot:
             return
-        if "aero" in message.content.lower():
+        if aeromention in message.content.lower():
             async with message.channel.typing():
-                await message.reply("Aero says fuck you, and TA is fake news")
+                await message.channel.send(aeroanswer)
             return
