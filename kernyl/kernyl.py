00787557149e2d714313f6317669666fc289d3a8
Saim <ssimsek@smail.uni-koeln.de>
2022-03-24 00:13:18 +0100

    Update kernyl.py

--- kernyl/kernyl.py
+++ kernyl/kernyl.py
@@ -10,5 +10,5 @@ class Kernyl(commands.Cog):
     @commands.Cog.listener()
     async def on_message(message):
         if message.channel.id == 811566276039540746:
-            target_channel = self.bot.get_channel(956314205902999572)
+            target_channel = self.bot.get_channel(951076247042146314)
             await target_channel.send(embed=message.embeds[0])
