82279eae69e9b617635ea5b8443b83200c392cee
Saim <ssimsek@smail.uni-koeln.de>
2022-03-23 17:27:19 +0100

    Update kernyl.py

--- kernyl/kernyl.py
+++ kernyl/kernyl.py
@@ -6,30 +6,19 @@ class Kernyl(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        
+    
             
     @commands.Cog.listener()
-    
-    async def on_message(message):
-        if len(message.content) > 250 or message.author.bot:
-            return
-        if message.channel.id === "944022733262569562":
-            messageL = f"{message.author.name.replace(message.author.discriminator, '')} posted: '{message.content}'"
-            success1 = await SendHomeMML(messageL)
-        if success1 is None:
-                print("Message Log message failed.")
-            descE = f"{message.author.name.replace(message.author.discriminator, '')} posted: \n'{message.content}'\n" \
-                f"This was in a Guild titled '{message.guild.name}' within Channel '{message.channel.name}'\n"
-            MessageE = discord.Embed(title="Message Log", description=descE, colour=8421376)
-            MessageE.set_footer(text=f"Posted on: {message.created_at.isoformat(' ')}")
-            success2 = await SendHomeEML(MessageE)
-            if success2 is None:
-                print("Message Log embed failed.")
-            # and so on...
+async def on_message(message):
+    if message.channel.id == '944022733262569562':
+        author_name = message.author.name + "#" + message.author.discriminator
+        if len(message.attachments) > 0:
+            image_url = message.attachments[0].url
+        else:
+            image_url = ""
+        await send_message(build_embed(author_name, message.author.avatar_url_as(format=None, static_format='png', size=1024), message.clean_content, message.author.color, image_url))
 
-    #Some time later... #
-
-    async def SendHomeEML(embedded):
-        return await bot.get_channel(956215619777359892).send(embed=embedded)
-
-    async def SendHomeMML(message):
-        return await bot.get_channel(956215619777359892).send(content=discord.utils.escape_mentions(message))
+async def send_message(message_embed):
+    channel = target_client.get_channel('956215619777359892')
+    await channel.send(embed=message_embed)
