4c233bb51fb35e8d57806f1645daeb3a15f61c18
Saim <ssimsek@smail.uni-koeln.de>
2022-03-23 22:59:12 +0100

    Update kernyl.py

--- kernyl/kernyl.py
+++ kernyl/kernyl.py
@@ -2,14 +2,14 @@ import discord
 from redbot.core import commands
 
 class Kernyl(commands.Cog):
-    """My custom cog to troll Kernyl"""
+    """My custom cog"""
 
     def __init__(self, bot):
         self.bot = bot
    
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message):
-        if message.author.self.bot:
+        if message.author.bot:
             return
         if message.channel.id == 804524249464700942:
             target_channel = self.bot.get_channel(833785942636232755)
