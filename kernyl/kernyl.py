6408727d76e25ec6217136f8b6d8662475216bed
Saim <ssimsek@smail.uni-koeln.de>
2022-03-25 15:04:23 +0100

    Update kernyl.py

--- kernyl/kernyl.py
+++ kernyl/kernyl.py
@@ -11,4 +11,4 @@ class Kernyl(commands.Cog):
     async def on_message(message):
         if message.channel.id == 811566276039540746:
             target_channel = self.bot.get_channel(951076247042146314)
-            await target_channel.send(embed=message.embeds[0])
+            await target_channel.send(embed=message.embeds[0].copy())
