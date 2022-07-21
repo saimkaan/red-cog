85a2a64ef014c3a73098ccf3d64e9e7e6d397782
Saim <ssimsek@smail.uni-koeln.de>
2022-07-21 11:24:43 +0200

    Update kernyl.py

--- kernyl/kernyl.py
+++ kernyl/kernyl.py
@@ -8,7 +8,10 @@ class Kernyl(commands.Cog):
         self.bot = bot
    
     @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-    if message.channel.id == 804524249464700942:
-        target_channel = bot.get_channel(999601740561252372)
-        await target_channel.send(message.content)
+    async def on_message_without_command(self, message: discord.Message):
+        if message.author.user:
+            return
+        if message.channel.id == 804524249464700942:
+            target_channel = self.bot.get_channel(999601740561252372)
+            await target_channel.send(message.content)
+            await target_channel.send(embed=message.embeds[0])
