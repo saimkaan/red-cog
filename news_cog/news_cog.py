0288111f719210cd3f78088639635031a5a293ec
Saim <ssimsek@smail.uni-koeln.de>
2023-05-25 17:26:36 +0200

    Update news_cog.py

--- news_cog/news_cog.py
+++ news_cog/news_cog.py
@@ -6,7 +6,7 @@ import requests
 import datetime
 import pytz
 
-API_URL = 'https://phx.unusualwhales.com/api/news/headlines-feed?limit=10'
+API_URL = 'https://phx.unusualwhales.com/api/news/headlines-feed?limit=50'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
