246d9f4b54a5fff134e8fd15a6512469261db6e7
Saim Simsek <saimkaansimsek@gmail.com>
2023-05-29 17:05:52 +0200

    yes

--- testing/test.py
+++ testing/test.py
@@ -1,5 +1,9 @@
 import discord
-from redbot.core import commands, tasks
+import asyncio
+import aiohttp
+import datetime
+from redbot.core import commands
+
 
 # Creating a cog class
 class NewsFeed(commands.Cog):
@@ -8,9 +12,10 @@ class NewsFeed(commands.Cog):
     # Initializing the cog
     def __init__(self, bot):
         self.bot = bot
-        self.url = "https://phx.unusualwhales.com/api/news/headlines-feed?limit=50" # The URL to get the news from
-        self.data = None # The data structure to store the news
-        self.channels = {} # A dictionary to store the channels that should receive the news
+        self.url = "https://phx.unusualwhales.com/api/news/headlines-feed?limit=50"  # The URL to get the news from
+        self.data = None  # The data structure to store the news
+        self.channels = {}  # A dictionary to store the channels that should receive the news
+        self.check_news_task = None
 
     # A command to add a channel to receive news
     @commands.command()
@@ -37,38 +42,39 @@ class NewsFeed(commands.Cog):
         await ctx.send(f"Removed {channel.mention} from receiving news.")
 
     # A background task that runs every 5 seconds
-    @tasks.loop(seconds=5)
     async def check_news(self):
         """Checks for new news from the website and posts them to the channels."""
-        # Get the data from the URL as JSON
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.url) as response:
-                data = await response.json()
-        # Check if there is any data
-        if not data["data"]:
-            return
-        # Check if there is any new data
-        if data == self.data:
-            return
-        # Store the new data
-        self.data = data
-        # Loop through each news item in reverse order (to post oldest first)
-        for item in reversed(data["data"]):
-            # Create an embed with the news information
-            embed = discord.Embed(title=item["headline"], color=discord.Color.blue())
-            embed.set_footer(text=f"Source: {item['source']}")
-            embed.timestamp = datetime.datetime.fromisoformat(item["created_at"])
-            # Loop through each channel in the dictionary
-            for channel_id, guild_id in self.channels.items():
-                # Get the channel object from the bot
-                channel = self.bot.get_channel(channel_id)
-                # Check if the channel exists and is in the same guild as stored
-                if channel and channel.guild.id == guild_id:
-                    # Send the embed to the channel
-                    await channel.send(embed=embed)
+        while True:
+            await asyncio.sleep(5)  # Wait for 5 seconds
+            # Get the data from the URL as JSON (you need to import aiohttp for this)
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url) as response:
+                    data = await response.json()
+            # Check if there is any data
+            if not data["data"]:
+                continue
+            # Check if there is any new data
+            if data == self.data:
+                continue
+            # Store the new data
+            self.data = data
+            # Loop through each news item in reverse order (to post oldest first)
+            for item in reversed(data["data"]):
+                # Create an embed with the news information
+                embed = discord.Embed(title=item["headline"], color=discord.Color.blue())
+                embed.set_footer(text=f"Source: {item['source']}")
+                embed.timestamp = datetime.datetime.fromisoformat(item["created_at"])
+                # Loop through each channel in the dictionary
+                for channel_id, guild_id in self.channels.items():
+                    # Get the channel object from the bot
+                    channel = self.bot.get_channel(channel_id)
+                    # Check if the channel exists and is in the same guild as stored
+                    if channel and channel.guild.id == guild_id:
+                        # Send the embed to the channel
+                        await channel.send(embed=embed)
 
     # A listener that starts the background task when the bot is ready
     @commands.Cog.listener()
     async def on_ready(self):
         """Starts the background task when the bot is ready."""
-        self.check_news.start()
+        self.check_news_task
