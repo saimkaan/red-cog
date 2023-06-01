35502ccfa4e2b8c6348ac43b5b8811234dcacfc1
Saim <ssimsek@smail.uni-koeln.de>
2023-06-01 10:02:23 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,81 +1,42 @@
-from redbot.core import commands, Config
-import aiohttp
-import asyncio
-import discord
+from redbot.core import commands
 import datetime
-import pytz
 
-class NewsFeed(commands.Cog):
+class DailyMessage(commands.Cog):
+    """A cog that posts a daily message to a Discord channel."""
+
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1231231337)
-        default_guild = {"channels": []}
-        self.config.register_guild(**default_guild)
-        self.session = aiohttp.ClientSession()
-        self.url = "https://phx.unusualwhales.com/api/news/headlines-feed?limit=10"
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        self.data = []
-        self.task = asyncio.create_task(self.fetch_data())
-
-    def cog_unload(self):
-        if self.task:
-            self.task.cancel()
-            self.task = None
-        asyncio.create_task(self.session.close())
-
+        self.channel = None
+        self.message = None
 
     @commands.group()
-    async def newsfeed(self, ctx):
+    async def daily(self, ctx):
+        """Manage the daily message settings."""
         pass
 
-    @newsfeed.command()
-    async def setchannel(self, ctx, channel: discord.TextChannel):
-        async with self.config.guild(ctx.guild).channels() as channels:
-            if channel.id in channels:
-                await ctx.send(f"{channel.mention} is already a news feed channel.")
-                return
-            channels.append(channel.id)
-            await ctx.send(f"{channel.mention} set as a news feed channel.")
+    @daily.command()
+    async def channel(self, ctx, channel: discord.TextChannel):
+        """Set the channel where the daily message will be posted."""
+        self.channel = channel
+        await ctx.send(f"Channel set to {channel.mention}.")
 
-    @newsfeed.command()
-    async def removechannel(self, ctx, channel: discord.TextChannel):
-        async with self.config.guild(ctx.guild).channels() as channels:
-            if channel.id not in channels:
-                await ctx.send(f"{channel.mention} is not a news feed channel.")
-                return
-            channels.remove(channel.id)
-            await ctx.send(f"{channel.mention} removed as a news feed channel.")
+    @daily.command()
+    async def message(self, ctx, *, message: str):
+        """Set the message that will be posted daily."""
+        self.message = message
+        await ctx.send(f"Message set to {message}.")
 
-    @newsfeed.command()
-    async def listchannels(self, ctx):
-        channels = await self.config.guild(ctx.guild).channels()
-        if not channels:
-            await ctx.send("No news feed channels set.")
-            return
-        channel_mentions = [f"<#{channel_id}>" for channel_id in channels]
-        await ctx.send(f"News feed channels: {', '.join(channel_mentions)}")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """Schedule the daily message task when the bot is ready."""
+        self.bot.loop.create_task(self.daily_message_task())
 
-    async def fetch_data(self):
-        while True:
-            try:
-                async with self.session.get(self.url, headers=self.headers) as resp:
-                    json_data = await resp.json()
-                    new_data = [item for item in json_data["data"] if item not in self.data and item["is_major"]]
-                    if new_data:
-                        for guild in self.bot.guilds:
-                            channels = await self.config.guild(guild).channels()
-                            for channel_id in channels:
-                                channel = guild.get_channel(channel_id)
-                                for item in new_data:
-                                    embed = discord.Embed(title=item["headline"], timestamp=datetime.datetime.now(pytz.utc))
-                                    embed.set_footer(text=item["source"])
-                                    if item["tickers"]:
-                                        tickers = ", ".join(item["tickers"])
-                                        embed.add_field(name="Tickers", value=tickers)
-                                    await channel.send(embed=embed)
-                        self.data.extend(new_data)
-                await asyncio.sleep(5)
-            except Exception as e:
-                print(e)
-                await asyncio.sleep(60)
-
+    async def daily_message_task(self):
+        """A task that runs every day and posts the daily message."""
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed():
+            now = datetime.datetime.utcnow()
+            if now.hour == 19 and now.minute == 1: # 12:01 PST
+                if self.channel and self.message:
+                    await self.channel.send(self.message)
+            await asyncio.sleep(60) # wait for a minute before checking again
