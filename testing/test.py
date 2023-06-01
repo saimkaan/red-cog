263dd79eb87fe315ce5cc6164ebb1facd86589e4
Saim <ssimsek@smail.uni-koeln.de>
2023-06-01 10:12:30 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,4 +1,5 @@
 from redbot.core import commands
+from redbot.core import Config
 import datetime
 import discord
 
@@ -7,8 +8,8 @@ class DailyMessage(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.channel = None
-        self.message = None
+        self.config = Config.get_conf(self, identifier=198762736482)
+        self.config.register_global(channel=None, message=None)
 
     @commands.group()
     async def daily(self, ctx):
@@ -18,13 +19,13 @@ class DailyMessage(commands.Cog):
     @daily.command()
     async def channel(self, ctx, channel: discord.TextChannel):
         """Set the channel where the daily message will be posted."""
-        self.channel = channel
+        await self.config.channel.set(channel.id)
         await ctx.send(f"Channel set to {channel.mention}.")
 
     @daily.command()
     async def message(self, ctx, *, message: str):
         """Set the message that will be posted daily."""
-        self.message = message
+        await self.config.message.set(message)
         await ctx.send(f"Message set to {message}.")
 
     @commands.Cog.listener()
@@ -33,12 +34,15 @@ class DailyMessage(commands.Cog):
         self.bot.loop.create_task(self.daily_message_task())
 
     async def daily_message_task(self):
-    """A task that runs every day and posts the daily message."""
-    await self.bot.wait_until_ready()
-    while not self.bot.is_closed():
-        now = datetime.datetime.utcnow()
-        print(now)
-        if now.hour == 9 and now.minute == 1: # 09:01 bot time
-            if self.channel and self.message:
-                await self.channel.send(self.message)
-        await asyncio.sleep(60) # wait for a minute before checking again
+        """A task that runs every day and posts the daily message."""
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed():
+            now = datetime.datetime.utcnow()
+            print(now)
+            if now.hour == 9 and now.minute == 1: # 09:01 bot time
+                channel_id = await self.config.channel()
+                message = await self.config.message()
+                if channel_id and message:
+                    channel = self.bot.get_channel(channel_id)
+                    await channel.send(message)
+            await asyncio.sleep(60) # wait for a minute before checking again
