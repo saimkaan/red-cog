8f57c0f2f811c704af899c8142003ecd8b207835
Saim <ssimsek@smail.uni-koeln.de>
2023-10-23 23:53:12 +0200

    yes

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -11,16 +11,10 @@ class VxTwitter(commands.Cog):
             return
 
         if "twitter.com" in message.content:
-            new_message = re.sub(r"(https?://)?(www\.)?twitter\.com/(\w+)/status/(\d+)", r"https://vxtwitter.com/\3/status/\4", message.content)
-            if "vxtwitter.com" in new_message or "fixvx.com" in new_message:
-                await message.channel.send(f"{new_message} from: {message.author.mention}")
-            else:
-                await message.channel.send(f"{new_message} from: {message.author.mention}")
-                await message.delete()
+            new_message = re.sub(r"(https?://)?(www\.)?twitter\.com/(\w+)/status/(\d+)(?!vxtwitter\.com)", r"https://vxtwitter.com/\3/status/\4", message.content)
+            await message.channel.send(f"{new_message} from: {message.author.mention}")
+            await message.delete()
         if "x.com" in message.content:
-            new_message = re.sub(r"(https?://)?(www\.)?x\.com/(\w+)/status/(\d+)", r"https://fixvx.com/\3/status/\4", message.content)
-            if "vxtwitter.com" in new_message or "fixvx.com" in new_message:
-                await message.channel.send(f"{new_message} from: {message.author.mention}")
-            else:
-                await message.channel.send(f"{new_message} from: {message.author.mention}")
-                await message.delete()
+            new_message = re.sub(r"(https?://)?(www\.)?x\.com/(\w+)/status/(\d+)(?!fixvx\.com)", r"https://fixvx.com/\3/status/\4", message.content)
+            await message.channel.send(f"{new_message} from: {message.author.mention}")
+            await message.delete()
