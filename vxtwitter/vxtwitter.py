d2276107ea8be99a06ad0593af967fc47e0584b6
Saim <ssimsek@smail.uni-koeln.de>
2023-10-24 00:01:44 +0200

    yes

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -11,10 +11,10 @@ class VxTwitter(commands.Cog):
             return
 
         if "twitter.com" in message.content:
-            new_message = re.sub(r"(https?://)?(www\.)?twitter\.com/(?!vxtwitter\.com)(\w+)/status/(\d+)", r"https://vxtwitter.com/\3/status/\4", message.content)
+            new_message = re.sub(r"(https?://)?(www\.)?twitter\.com/(\w+)/status/(\d+)", r"https://vxtwitter.com/\3/status/\4", message.content)
             await message.channel.send(f"{new_message} from: {message.author.mention}")
             await message.delete()
         if "x.com" in message.content:
-            new_message = re.sub(r"(https?://)?(www\.)?x\.com/(?!fixvx\.com)(\w+)/status/(\d+)", r"https://fixvx.com/\3/status/\4", message.content)
+            new_message = re.sub(r"(https?://)?(www\.)?x\.com/(\w+)/status/(\d+)", r"https://fixvx.com/\3/status/\4", message.content)
             await message.channel.send(f"{new_message} from: {message.author.mention}")
             await message.delete()
