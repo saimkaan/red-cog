4188b3693cbcc04867ce5fc9b823e88302494fa6
Saim <ssimsek@smail.uni-koeln.de>
2023-10-26 16:17:37 +0200

    yes

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -1,5 +1,6 @@
 from redbot.core import commands
 import re
+import time
 
 class VxTwitter(commands.Cog):
     def __init__(self, bot):
@@ -11,11 +12,13 @@ class VxTwitter(commands.Cog):
             return
 
         if "twitter.com" in message.content:
+            time.sleep(1)
             new_message = re.sub(r"(https?://)?(www\.)?twitter\.com/(\w+)/status/(\d+)", r"https://vxtwitter.com/\3/status/\4", message.content)
             if len(message.embeds) == 0:
                 await message.channel.send(f"{new_message} from: {message.author.mention}")
                 await message.delete()
         if "x.com" in message.content:
+            time.sleep(1)
             new_message = re.sub(r"(https?://)?(www\.)?x\.com/(\w+)/status/(\d+)", r"https://fixvx.com/\3/status/\4", message.content)
             if len(message.embeds) == 0:
                 await message.channel.send(f"{new_message} from: {message.author.mention}")
