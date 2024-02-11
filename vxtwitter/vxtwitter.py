b844a4397f11ad24cefba9f7159e9ea677620cea
Saim <ssimsek@smail.uni-koeln.de>
2024-02-11 22:08:38 +0100

    Update vxtwitter.py

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -33,3 +33,13 @@ class VxTwitter(commands.Cog):
             new_message = re.sub(r"(https?://)?(www\.)?instagram\.com/(.*)", r"https://www.ddinstagram.com/\3", message.content)
             await message.channel.send(f"{new_message} from: {message.author.mention}")
             await message.delete()
+
+        # New command to convert Twitter video links
+        if message.content.startswith("!video"):
+            # Extract the Twitter video link
+            match = re.match(r"!video (https?://twitter\.com/[^/]+/status/\d+)", message.content)
+            if match:
+                twitter_link = match.group(1)
+                converted_link = re.sub(r"https?://twitter\.com/([^/]+)/status/(\d+)", r"https://d.fxtwitter.com/\1/status/\2", twitter_link)
+                await message.channel.send(f"Converted link: {converted_link} from: {message.author.mention}")
+                await message.delete()
