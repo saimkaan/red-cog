59a06bf97061a2ec1098d6c8c57e69d9d3a615cc
Saim <ssimsek@smail.uni-koeln.de>
2024-02-11 16:05:26 +0100

    Update vxtwitter.py

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -21,3 +21,8 @@ class VxTwitter(commands.Cog):
             if len(message.embeds) == 0:
                 await message.channel.send(f"{new_message} from: {message.author.mention}")
                 await message.delete()
+        if "tiktok.com" in message.content and "vxtiktok.com" not in message.content:
+            new_message = re.sub(r"(https?://)?(www\.)?tiktok\.com/(\w+)/status/(\d+)", r"https://vxtiktok.com/\3/status/\4", message.content)
+            if len(message.embeds) == 0:
+                await message.channel.send(f"{new_message} from: {message.author.mention}")
+                await message.delete()
