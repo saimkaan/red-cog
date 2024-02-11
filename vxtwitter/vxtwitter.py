5000014d5a44e0eccb04969a90a94e6bb419417d
Saim <ssimsek@smail.uni-koeln.de>
2024-02-11 16:28:09 +0100

    Update vxtwitter.py

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -13,20 +13,17 @@ class VxTwitter(commands.Cog):
         # Twitter link replacement (unchanged)
         if "twitter.com" in message.content and "vxtwitter.com" not in message.content:
             new_message = re.sub(r"(https?://)?(www\.)?twitter\.com/(\w+)/status/(\d+)", r"https://vxtwitter.com/\3/status/\4", message.content)
-            if len(message.embeds) == 0:
-                await message.channel.send(f"{new_message} from: {message.author.mention}")
-                await message.delete()
+            await message.channel.send(f"{new_message} from: {message.author.mention}")
+            await message.delete()
 
         # TikTok link replacement (added)
         if "tiktok.com" in message.content and "vxtiktok.com" not in message.content:
             new_message = re.sub(r"(https?://)?(www\.)?tiktok\.com/(@\w+)/video/(\w+)", r"https://vxtiktok.com/\3/video/\4", message.content)
-            if len(message.embeds) == 0:
-                await message.channel.send(f"{new_message} from: {message.author.mention}")
-                await message.delete()
+            await message.channel.send(f"{new_message} from: {message.author.mention}")
+            await message.delete()
 
         # Existing code for x.com links (unchanged)
         if "x.com" in message.content and "fixvx.com" not in message.content:
             new_message = re.sub(r"(https?://)?(www\.)?x\.com/(\w+)/status/(\d+)", r"https://fixvx.com/\3/status/\4", message.content)
-            if len(message.embeds) == 0:
-                await message.channel.send(f"{new_message} from: {message.author.mention}")
-                await message.delete()
+            await message.channel.send(f"{new_message} from: {message.author.mention}")
+            await message.delete()
