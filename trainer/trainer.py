8133e39442704211dd19e7fe4b2f9ba43a045a59
Saim <ssimsek@smail.uni-koeln.de>
2024-04-24 18:39:17 +0200

    Update trainer.py

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,13 +1,8 @@
-from redbot.core import commands, Config
 import aiohttp
 import asyncio
 import discord
-import datetime
-import pytz
-import requests
 import logging
-import threading
-import time
+from redbot.core import commands, Config
 
 class Trainer(commands.Cog):
     def __init__(self, bot):
@@ -18,11 +13,10 @@ class Trainer(commands.Cog):
         self.session = aiohttp.ClientSession()
         self.headers = {
             "accept": "*/*",
-            "x-api-key": "1d336873-3714-504d-ade9-e0017bc7f390"
+            "x-api-key": "YOUR_API_KEY"
         }
         self.url_reservoir = "https://api.reservoir.tools/orders/asks/v5?tokenSetId=contract%3A0x8a3749936e723325c6b645a0901470cd9e790b94&limit=10"
         self.url_trainer = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
-        self.url_floor_ask = "https://api.reservoir.tools/events/collections/floor-ask/v2?collection=0x8a3749936e723325c6b645a0901470cd9e790b94&limit=1"
         self.data = []
         self.task = asyncio.create_task(self.fetch_data())
         self.last_message_time = {}
@@ -67,114 +61,56 @@ class Trainer(commands.Cog):
     async def fetch_data(self):
         while True:
             try:
-                threshold_price = self.get_threshold_price()
-                if threshold_price is not None:
-                    token_ids = self.fetch_reservoir_data(threshold_price)
-                    if token_ids:
-                        self.fetch_trainer_data_with_threads(token_ids)
-                await asyncio.sleep(15)  # Run every 15 seconds
+                async with self.session.get(self.url_reservoir, headers=self.headers) as response:
+                    data = await response.json()
+                    if 'orders' in data:
+                        for order in data['orders']:
+                            price_eth = order['price']['amount']['raw']
+                            price_eth_decimal = int(price_eth) / (10 ** 18)
+                            token_id = order['criteria']['data']['token']['tokenId']
+                            await self.check_and_post_message(token_id, price_eth_decimal)
             except Exception as e:
                 logging.error(f"Error occurred while fetching data: {e}")
-                await asyncio.sleep(60)
+            await asyncio.sleep(15)  # Run every 15 seconds
 
-    def fetch_trainer_data(self, trainer_id):
-        try:
-            payload = {'nftType': 'trainer', 'tokenId': str(trainer_id)}
-            response = requests.post(self.url_trainer, json=payload)
-            data = response.json()
-            if 'result' in data and 'response' in data['result']:
-                relics_response = data['result']['response']['relicsResponse']
-                for relic in relics_response:
-                    if relic['relicsType'] in ['gold', 'diamond'] and relic['count'] > 0:
-                        return {
-                            'relics_type': relic['relicsType'],
-                            'relics_count': relic['count']
-                        }
-        except Exception as e:
-            logging.error(f"Error occurred while fetching data from Trainer API: {e}")
-        return None
-
-    def fetch_reservoir_data(self, threshold_price):
-        try:
-            response = requests.get(self.url_reservoir, headers=self.headers)
-            data = response.json()
-            if 'orders' in data:
-                token_ids = []
-                for order in data['orders']:
-                    # Parse price data
-                    price_eth = order['price']['amount']['raw']
-                    # Convert to decimal ETH value
-                    price_eth_decimal = int(price_eth) / (10 ** 18)
-                    token_id = order['criteria']['data']['token']['tokenId']
-                    if price_eth_decimal < threshold_price:
-                        token_ids.append((token_id, price_eth_decimal))
-                return token_ids
-        except Exception as e:
-            logging.error(f"Error occurred while fetching data from Reservoir API: {e}")
-        return None
-
-    def filter_token_ids(self, token_ids, threshold_price):
-        # Filter token IDs based on price criteria
-        filtered_token_ids = []
-        for token_id, price in token_ids:
-            if price <= threshold_price * 1.2:  # 20% higher than the floor price for diamond relics
-                filtered_token_ids.append((token_id, price))
-            elif price <= threshold_price * 1.05:  # 10% higher than the floor price for gold relics
-                # Check if the relic type is gold
-                trainer_data = self.fetch_trainer_data(token_id)
-                if trainer_data and trainer_data['relics_type'] == 'gold':
-                    filtered_token_ids.append((token_id, price))
-        return filtered_token_ids
-
-
-    def fetch_trainer_data_with_threads(self, token_ids):
-        loop = asyncio.get_event_loop()
-        for token_id, _ in token_ids:
-            asyncio.run_coroutine_threadsafe(self.fetch_and_print_trainer_data(token_id), loop)
-    
-    async def fetch_and_print_trainer_data(self, token_id):
-        trainer_data = self.fetch_trainer_data(token_id)
-        if trainer_data:
-            # Check if the trainer ID has exceeded the message limit
-            if self.check_message_limit(token_id):
-                # Construct the OpenSea link with the trainer ID
-                blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
-                message = f"@everyone {trainer_data['relics_type']} relic count: {trainer_data['relics_count']}\n{blur_link}"
+    async def check_and_post_message(self, token_id, price):
+        threshold_price = await self.calculate_threshold_price(token_id)
+        if threshold_price is not None and price <= threshold_price:
+            trainer_data = await self.fetch_trainer_data(token_id)
+            if trainer_data:
+                message = f"@everyone {trainer_data['relics_type']} relic count: {trainer_data['relics_count']}\n{token_id}"
                 for guild in self.bot.guilds:
                     channels = await self.config.guild(guild).channels()
                     for channel_id in channels:
                         channel = guild.get_channel(channel_id)
                         await channel.send(message)
-                # Update the last message time for the trainer ID
-                self.update_last_message_time(token_id)
-            else:
-                pass
-        else:
-            pass
 
+    async def calculate_threshold_price(self, token_id):
+        try:
+            async with self.session.post(self.url_trainer, json={'nftType': 'trainer', 'tokenId': str(token_id)}) as response:
+                data = await response.json()
+                if 'result' in data and 'response' in data['result']:
+                    relics_response = data['result']['response']['relicsResponse']
+                    total_relics_count = sum(relic['count'] for relic in relics_response if relic['relicsType'] in ['gold', 'diamond'])
+                    # Adjust threshold based on relics count
+                    threshold_price = total_relics_count * 0.15  # 0.15 increase for each relic
+                    return threshold_price
+        except Exception as e:
+            logging.error(f"Error occurred while calculating threshold price: {e}")
+        return None
 
-
-    def check_message_limit(self, token_id):
-        # Check if the trainer ID has exceeded the message limit (2 messages per hour)
-        current_time = time.time()
-        last_message_time = self.last_message_time.get(token_id, 0)
-        if current_time - last_message_time >= 3600:  # 3600 seconds = 1 hour
-            # Reset the message count if the time limit has elapsed
-            self.last_message_time[token_id] = current_time
-            return True
-        else:
-            # Check if the message count for the trainer ID exceeds 2
-            return self.last_message_time.get(f"{token_id}_count", 0) < 2
-
-    def update_last_message_time(self, token_id):
-        # Update the last message time for the trainer ID
-        current_time = time.time()
-        self.last_message_time[token_id] = current_time
-        # Increment the message count for the trainer ID
-        self.last_message_time[f"{token_id}_count"] = self.last_message_time.get(f"{token_id}_count", 0) + 1
-
-    def cog_unload(self):
-        if self.task:
-            self.task.cancel()
-            self.task = None
-        asyncio.create_task(self.session.close())
+    async def fetch_trainer_data(self, token_id):
+        try:
+            async with self.session.post(self.url_trainer, json={'nftType': 'trainer', 'tokenId': str(token_id)}) as response:
+                data = await response.json()
+                if 'result' in data and 'response' in data['result']:
+                    relics_response = data['result']['response']['relicsResponse']
+                    for relic in relics_response:
+                        if relic['relicsType'] in ['gold', 'diamond'] and relic['count'] > 0:
+                            return {
+                                'relics_type': relic['relicsType'],
+                                'relics_count': relic['count']
+                            }
+        except Exception as e:
+            logging.error(f"Error occurred while fetching data from Trainer API: {e}")
+        return None
