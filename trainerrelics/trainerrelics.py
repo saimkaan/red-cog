2f77440346c54952c8e6ee3ca327f2721380f1dd
Saim <testing@gmail.com>
2024-04-25 19:40:00 +0200

    123123

--- trainerrelics/trainerrelics.py
+++ trainerrelics/trainerrelics.py
@@ -13,11 +13,11 @@ class TrainerRelics(commands.Cog):
 
     @trainerrelics.command()
     async def get(self, ctx, token_id: int):
-        await self.fetch_and_print_trainer_data(ctx, token_id)
+        await self.fetch_and_print_trainerrelics_data(ctx, token_id)
 
     async def fetch_and_print_trainerrelics_data(self, ctx, token_id):
-        trainerrelics_data = self.fetch_trainerrelicsr_data(token_id)
-        if trainer_data:
+        trainerrelics_data = self.fetch_trainerrelics_data(token_id)
+        if trainerrelics_data:
             blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
             message = f"{trainerrelics_data['relics_type']} relic count: {trainerrelics_data['relics_count']}\n{blur_link}"
             await ctx.send(message)
@@ -27,7 +27,7 @@ class TrainerRelics(commands.Cog):
     def fetch_trainerrelics_data(self, trainerrelics_id):
         try:
             payload = {'nftType': 'trainer', 'tokenId': str(trainerrelics_id)}
-            response = requests.post(self.url_trainerrelics, json=payload)
+            response = requests.post(self.url_trainer, json=payload)
             data = response.json()
             if 'result' in data and 'response' in data['result']:
                 relics_response = data['result']['response']['relicsResponse']
