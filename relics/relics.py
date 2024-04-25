f09ab281b547a0ca5aff176ba55a85c6861732c2
Saim <testing@gmail.com>
2024-04-25 19:50:28 +0200

    123

--- relics/relics.py
+++ relics/relics.py
@@ -2,7 +2,7 @@ from redbot.core import commands
 import requests
 import logging
 
-class Relics(commands.Cog):
+class TrainerRelics(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.url_relics = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
@@ -22,7 +22,10 @@ class Relics(commands.Cog):
     async def fetch_and_print_relics_data(self, ctx, token_id, nft_type):
         relics_data = self.fetch_relics_data(token_id, nft_type)
         if relics_data:
-            blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
+            if nft_type == 'pixelmon':
+                blur_link = f"https://blur.io/asset/0x32973908faee0bf825a343000fe412ebe56f802a/{token_id}"
+            else:
+                blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
             relics_message = "\n".join([f"{relic['relicsType']} relic count: {relic['count']}" for relic in relics_data['relics']])
             message = f"{relics_message}\n{blur_link}"
             await ctx.send(message)
