7c9c4fccb99de38126053f0b09d65eb9356af625
Saim <testing@gmail.com>
2024-04-26 18:52:10 +0200

    123

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -18,8 +18,7 @@ class Trainer(commands.Cog):
             "x-api-key": "1d336873-3714-504d-ade9-e0017bc7f390"
         }
         self.url_reservoir = "https://api.reservoir.tools/orders/asks/v5?tokenSetId=contract%3A0x8a3749936e723325c6b645a0901470cd9e790b94&limit=10"
-        self.url_trainer = "https://api-cp.pixelmon.ai/nft/get-relics-count"
-        self.url_floor_price = "https://api.reservoir.tools/events/collections/floor-ask/v2?collection=0x8a3749936e723325c6b645a0901470cd9e790b94&limit=1"
+        self.url_trainer = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
         self.data = []
         self.task = asyncio.create_task(self.fetch_data())
         self.last_message_time = {}
@@ -66,8 +65,8 @@ class Trainer(commands.Cog):
             try:
                 token_ids = self.fetch_reservoir_data()
                 if token_ids:
-                    self.fetch_trainer_data_with_async(token_ids)
-                await asyncio.sleep(30)
+                    self.fetch_trainer_data_with_threads(token_ids)
+                await asyncio.sleep(30)  # Run every 30 seconds
             except Exception as e:
                 logging.error(f"Error occurred while fetching data: {e}")
                 await asyncio.sleep(60)
@@ -79,18 +78,16 @@ class Trainer(commands.Cog):
                 data = await response.json()
                 if 'result' in data and 'response' in data['result']:
                     relics_response = data['result']['response']['relicsResponse']
-                    return relics_response
+                    for relic in relics_response:
+                        if relic['relicsType'] in ['diamond'] and relic['count'] > 0:
+                            return {
+                                'relics_type': relic['relicsType'],
+                                'relics_count': relic['count']
+                            }
         except Exception as e:
             logging.error(f"Error occurred while fetching data from trainer API: {e}")
         return None
 
-    async def fetch_floor_price(self):
-        async with self.session.get(self.url_floor_price, headers=self.headers) as response:
-            data = await response.json()
-            if 'events' in data and len(data['events']) > 0:
-                return data['events'][0]['floorAsk']['price']['decimal']
-            else:
-                return None
 
     def fetch_reservoir_data(self):
         try:
@@ -107,48 +104,47 @@ class Trainer(commands.Cog):
             logging.error(f"Error occurred while fetching data from Reservoir API: {e}")
         return None
 
-    async def fetch_trainer_data_with_async(self, token_data):
+    def fetch_trainer_data_with_threads(self, token_data):
+        loop = asyncio.get_event_loop()
         for data in token_data:
-            await self.fetch_and_print_trainer_data(data['token_id'], data['decimal_value'])
-
+            asyncio.run_coroutine_threadsafe(self.fetch_and_print_trainer_data(data['token_id'], data['decimal_value']), loop)
 
     async def fetch_and_print_trainer_data(self, token_id, decimal_value):
-        floor_price = await self.fetch_floor_price()
-        if floor_price is None:
-            logging.error("Error fetching floor price")
-            return
-        if decimal_value > floor_price * 2:
-            return
-        relics_data = await self.fetch_trainer_data(token_id)
-        if relics_data:
-            for relic in relics_data:
-                if (relic['relics_type'] == 'gold' and relic['count'] >= 2) or (relic['relics_type'] == 'diamond' and relic['count'] >= 1):
-                    if self.check_message_limit(token_id):
-                        blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
-                        message = f"@everyone {relic['relics_type']} relic count: {relic['count']}, Price: {decimal_value} ETH\n{blur_link}"
-                        for guild in self.bot.guilds:
-                            channels = await self.config.guild(guild).channels()
-                            for channel_id in channels:
-                                channel = guild.get_channel(channel_id)
-                                await channel.send(message)
-                        self.update_last_message_time(token_id)
-                    else:
-                        pass
-                else:
-                    pass
+        trainer_data = await self.fetch_trainer_data(token_id)
+        if trainer_data:
+            # Check if the trainer ID has exceeded the message limit
+            if self.check_message_limit(token_id):
+                # Construct the OpenSea link with the trainer ID
+                blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
+                message = f"@everyone {trainer_data['relics_type']} relic count: {trainer_data['relics_count']}, Price: {decimal_value} ETH\n{blur_link}"
+                for guild in self.bot.guilds:
+                    channels = await self.config.guild(guild).channels()
+                    for channel_id in channels:
+                        channel = guild.get_channel(channel_id)
+                        await channel.send(message)
+                # Update the last message time for the trainer ID
+                self.update_last_message_time(token_id)
+            else:
+                pass
+        else:
+            pass
 
     def check_message_limit(self, token_id):
+        # Check if the trainer ID has exceeded the message limit (1 message per 24 hours)
         current_time = time.time()
         last_message_time = self.last_message_time.get(token_id, 0)
-        if current_time - last_message_time >= 86400:
+        if current_time - last_message_time >= 86400:  # 86400 seconds = 24 hours
+            # Reset the message time if the time limit has elapsed
             self.last_message_time[token_id] = current_time
             return True
         else:
-            return False
+            return False  # Return False to indicate message limit exceeded
 
     def update_last_message_time(self, token_id):
+        # Update the last message time for the trainer ID
         current_time = time.time()
         self.last_message_time[token_id] = current_time
+        # Increment the message count for the trainer ID
         self.last_message_time[f"{token_id}_count"] = self.last_message_time.get(f"{token_id}_count", 0) + 1
 
     def cog_unload(self):
