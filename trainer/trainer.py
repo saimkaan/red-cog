5b649d72a0cdd60dc52ae315c916c707ee191589
Saim <testing@gmail.com>
2024-04-29 16:36:07 +0200

    123

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -89,6 +89,12 @@ class Trainer(commands.Cog):
     async def fetch_and_print_trainer_data(self, token_id, decimal_value):
         last_decimal_value = self.last_decimal_values.get(token_id)
         if last_decimal_value is None or last_decimal_value != decimal_value:
+            logging.info(f"New message for token ID {token_id} with decimal value {decimal_value}")
+        # Rest of your code for posting the message
+            self.last_decimal_values[token_id] = decimal_value  # Update last_decimal_values
+        else:
+            logging.info(f"Message for token ID {token_id} with decimal value {decimal_value} already posted, skipping.")
+        if last_decimal_value is None or last_decimal_value != decimal_value:
             trainer_data = await self.fetch_trainer_data(token_id)
             if trainer_data:
                 blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
