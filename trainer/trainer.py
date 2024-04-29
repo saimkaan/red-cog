c1f29175c9b2aef17891d61f5e1b302fa2c27cab
Saim <testing@gmail.com>
2024-04-29 16:25:02 +0200

    123

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -22,7 +22,7 @@ class Trainer(commands.Cog):
         self.url_attribute = "https://api.reservoir.tools/collections/0x8a3749936e723325c6b645a0901470cd9e790b94/attributes/explore/v5?tokenId={}&attributeKey=rarity"
         self.task = asyncio.create_task(self.fetch_data())
         self.last_decimal_values = {}
-        self.trainer_data_cache = {}
+        self.trainer_cache = {}
 
     @commands.group()
     async def trainer(self, ctx):
@@ -89,42 +89,35 @@ class Trainer(commands.Cog):
     async def fetch_and_print_trainer_data(self, token_id, decimal_value):
         last_decimal_value = self.last_decimal_values.get(token_id)
         if last_decimal_value is None or last_decimal_value != decimal_value:
-            trainer_data_info = self.trainer_data_cache.get(token_id)  # Check cache first
-            if trainer_data_info is None:
-                trainer_data_info = await self.fetch_trainer_data(token_id)
-                self.trainer_data_cache[token_id] = trainer_data_info  # Cache fetched data info
-            if trainer_data_info is not None:
-                fetched_data = trainer_data_info.get('data')
-                if fetched_data is not None:  # Data fetched successfully
-                    blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
-                    rarity_atts, floor_price = await self.get_attributes(token_id)
-                    if floor_price is not None:
-                        relics_value = self.calculate_relics_value(fetched_data)
-                        if relics_value >= 0.15:
-                            total_price = floor_price + relics_value
-                            relics_info = "\n".join([f"{relic_type.capitalize()} Relic Count: {count}" for relic_type, count in fetched_data.items()])
-                            message = f"@everyone\n**{rarity_atts['rarity']}** Trainer: {token_id}\n{relics_info}\nFloor Price: {floor_price:.4f} ETH\nRelics Value: {relics_value:.4f} ETH\n\n**Listing Price: {decimal_value:.4f} ETH**\n{blur_link}"
-                            if decimal_value <= total_price:
-                                self.last_decimal_values[token_id] = decimal_value
-                                for guild in self.bot.guilds:
-                                    channels = await self.config.guild(guild).channels()
-                                    for channel_id in channels:
-                                        channel = guild.get_channel(channel_id)
-                                        allowed_mentions = discord.AllowedMentions(everyone=True)
-                                        await channel.send(message, allowed_mentions=allowed_mentions)
-                else:
-                    # Trainer has no relics, cache it with a flag indicating no relics
-                    self.trainer_data_cache[token_id] = {'data': None, 'no_relics': True}
+            trainer_data = await self.fetch_trainer_data(token_id)
+            if trainer_data:
+                blur_link = f"https://blur.io/asset/0x8a3749936e723325c6b645a0901470cd9e790b94/{token_id}"
+                rarity_atts, floor_price = await self.get_attributes(token_id)
+                if floor_price is not None:
+                    relics_value = self.calculate_relics_value(trainer_data)
+                    if relics_value >= 0.15:
+                        total_price = floor_price + relics_value
+                        relics_info = "\n".join([f"{relic_type.capitalize()} Relic Count: {count}" for relic_type, count in trainer_data.items()])
+                        message = f"@everyone\n**{rarity_atts['rarity']}** Trainer: {token_id}\n{relics_info}\nFloor Price: {floor_price:.4f} ETH\nRelics Value: {relics_value:.4f} ETH\n\n**Listing Price: {decimal_value:.4f} ETH**\n{blur_link}"
+                        if decimal_value <= total_price:
+                            self.last_decimal_values[token_id] = decimal_value
+                            for guild in self.bot.guilds:
+                                channels = await self.config.guild(guild).channels()
+                                for channel_id in channels:
+                                    channel = guild.get_channel(channel_id)
+                                    allowed_mentions = discord.AllowedMentions(everyone=True)
+                                    await channel.send(message, allowed_mentions=allowed_mentions)
             else:
-                logging.error(f"Failed to fetch trainer data for Trainer ID: {token_id}")
-        else:
-            logging.info(f"Using cached trainer data for Trainer ID: {token_id}")
-
-
+                logging.error(f"No trainer data found for Trainer ID: {token_id}")
 
 
 
     async def fetch_trainer_data(self, trainer_id):
+        # Check if the trainer data is already in the cache
+        cached_data = self.trainer_cache.get(trainer_id)
+        if cached_data:
+            return cached_data
+        
         try:
             payload = {'nftType': 'trainer', 'tokenId': str(trainer_id)}
             async with self.session.post(self.url_trainer, json=payload) as response:
@@ -134,6 +127,10 @@ class Trainer(commands.Cog):
                     relics_data = {}
                     for relic in relics_response:
                         relics_data[relic['relicsType']] = relic['count']
+                    
+                    # Update cache with new data
+                    self.trainer_cache[trainer_id] = relics_data
+                    
                     return relics_data
         except Exception as e:
             logging.error(f"Error occurred while fetching data from trainer API: {e}")
@@ -150,7 +147,7 @@ class Trainer(commands.Cog):
         return None, None
 
     def calculate_relics_value(self, relics_data):
-        relic_values = {'diamond': 0.15, 'gold': 0.045, 'silver': 0.018, 'bronze': 0.009, 'wood': 1.0024}
+        relic_values = {'diamond': 0.15, 'gold': 0.045, 'silver': 0.018, 'bronze': 0.009, 'wood': 0.0024}
         total_value = 0
         for relic_type, count in relics_data.items():
             if relic_type in relic_values:
