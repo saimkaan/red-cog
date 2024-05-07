13f8b0bca4ed4b2bc37381bf8aea1ed104f19c8a
Saim <thegalbatronix@gmail.com>
2024-05-07 15:21:10 +0200

    123

--- snipe/snipe.py
+++ snipe/snipe.py
@@ -1,13 +1,14 @@
 from redbot.core import commands, Config
 import aiohttp
-import discord
 import asyncio
+import discord
 import logging
+import requests
 
-class Snipe(commands.Cog):
+class Trainer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=12344321)
+        self.config = Config.get_conf(self, identifier=222111)
         default_guild = {"channels": []}
         self.config.register_guild(**default_guild)
         self.session = aiohttp.ClientSession()
@@ -23,10 +24,10 @@ class Snipe(commands.Cog):
         self.task = asyncio.create_task(self.fetch_data_for_addresses())
 
     @commands.group()
-    async def snipe(self, ctx):
+    async def trainer(self, ctx):
         pass
 
-    @snipe.command()
+    @trainer.command()
     async def setchannel(self, ctx, channel: discord.TextChannel):
         async with self.config.guild(ctx.guild).channels() as channels:
             if channel.id in channels:
@@ -35,7 +36,7 @@ class Snipe(commands.Cog):
             channels.append(channel.id)
             await ctx.send(f"{channel.mention} set as a news feed channel.")
 
-    @snipe.command()
+    @trainer.command()
     async def removechannel(self, ctx, channel: discord.TextChannel):
         async with self.config.guild(ctx.guild).channels() as channels:
             if channel.id not in channels:
@@ -44,7 +45,7 @@ class Snipe(commands.Cog):
             channels.remove(channel.id)
             await ctx.send(f"{channel.mention} removed as a news feed channel.")
 
-    @snipe.command()
+    @trainer.command()
     async def listchannels(self, ctx):
         channels = await self.config.guild(ctx.guild).channels()
         if not channels:
@@ -52,19 +53,19 @@ class Snipe(commands.Cog):
             return
         channel_mentions = [f"<#{channel_id}>" for channel_id in channels]
         await ctx.send(f"News feed channels: {', '.join(channel_mentions)}")
-
+    
     async def fetch_data_for_addresses(self, guild):
         try:
             for address in self.addresses:
                 url = self.url_reservoir.format(address=address)
                 async with self.session.get(url, headers=self.headers) as response:
                     data = await response.json()
-                    for channel_id in await self.config.guild(guild).channels():
-                        channel = self.bot.get_channel(channel_id)
-                        if channel:
-                            await channel.send(f"Data for address {address}:\n{data}")
+                    logging.info(f"Data for address {address}:\n{data}")
         except Exception as e:
             logging.error(f"Error occurred while fetching data from Reservoir API: {e}")
 
     def cog_unload(self):
+        if self.task:
+            self.task.cancel()
+            self.task = None
         asyncio.create_task(self.session.close())
