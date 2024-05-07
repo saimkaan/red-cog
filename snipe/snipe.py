e854a538090a1364f3ebab35dd9143d5e9b1b90d
Saim <thegalbatronix@gmail.com>
2024-05-08 00:16:37 +0200

    123

--- snipe/snipe.py
+++ snipe/snipe.py
@@ -6,7 +6,7 @@ import discord
 class Snipe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=999888)
+        self.config = Config.get_conf(self, identifier=999888777)
         default_guild = {"channels": {}}
         self.config.register_guild(**default_guild)
         self.headers = {
@@ -20,7 +20,7 @@ class Snipe(commands.Cog):
             "trainer": "0x8a3749936e723325c6b645a0901470cd9e790b94",
             "pixelmon": "0x32973908faee0bf825a343000fe412ebe56f802a"
         }
-        self.task = asyncio.create_task(self.loop())
+        self.loop_task = None
 
     async def fetch_data(self, session, url):
         async with session.get(url, headers=self.headers) as response:
@@ -94,7 +94,7 @@ class Snipe(commands.Cog):
         await ctx.send(f"News feed channels: {', '.join(channel_mentions)}")
     
     @snipe.command()
-    async def loop(self, interval: int = 20):
+    async def loop(self, ctx, interval: int = 20):
         async with aiohttp.ClientSession() as session:
             while True:
                 try:
@@ -113,10 +113,23 @@ class Snipe(commands.Cog):
                     await asyncio.sleep(60)
                 else:
                     await asyncio.sleep(interval)
-    
-    def cog_unload(self):
-        if self.task:
-            self.task.cancel()
-            self.task = None
-        asyncio.create_task(self.bot.session.close())
 
+    @snipe.command()
+    async def start(self, ctx):
+        if self.loop_task is None or self.loop_task.done():
+            self.loop_task = asyncio.create_task(self.loop(ctx))
+
+    @snipe.command()
+    async def stop(self, ctx):
+        if self.loop_task and not self.loop_task.done():
+            self.loop_task.cancel()
+
+    @snipe.command()
+    async def unload(self, ctx):
+        if self.loop_task and not self.loop_task.done():
+            self.loop_task.cancel()
+        await self.bot.remove_cog("Snipe")
+        await ctx.send("Snipe cog has been unloaded.")
+
+def setup(bot):
+    bot.add_cog(Snipe(bot))
