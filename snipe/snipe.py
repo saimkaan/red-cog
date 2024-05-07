89a435bf38e01b8e9b431797b30e6a59f6ab1c22
Saim <thegalbatronix@gmail.com>
2024-05-07 22:43:01 +0200

    testing snipe

--- snipe/snipe.py
+++ snipe/snipe.py
@@ -1,96 +1,73 @@
 from redbot.core import commands, Config
-import aiohttp
 import asyncio
+import aiohttp
 import discord
-import logging
 
 class Snipe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=12344321)
+        self.config = Config.get_conf(self, identifier=111222)
         default_guild = {"channels": []}
         self.config.register_guild(**default_guild)
-        self.session = aiohttp.ClientSession()
         self.headers = {
             "accept": "*/*",
             "x-api-key": "1d336873-3714-504d-ade9-e0017bc7f390"
         }
-        self.nfttype = {
-            "trainer" : "0x8a3749936e723325c6b645a0901470cd9e790b94",
-            "pixelmon" : "0x32973908faee0bf825a343000fe412ebe56f802a"
+        self.relic_values = {'diamond': 0.15, 'gold': 0.045, 'silver': 0.018, 'bronze': 0.009, 'wood': 0.0024, 'unRevealed': 0.0024}
+        self.relics_log = {}
+        self.contract_address = {
+            "trainer": "0x8a3749936e723325c6b645a0901470cd9e790b94",
+            "pixelmon": "0x32973908faee0bf825a343000fe412ebe56f802a"
         }
-        self.url_reservoir = "https://api.reservoir.tools/orders/asks/v5?tokenSetId=contract%3A{address}&limit=20"
-        self.url_relics = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
-        self.data_list = []
-        self.relics_cache = {}
-        self.task = asyncio.create_task(self.run_task())
 
-    @commands.group()
+    async def fetch_data(self, session, url):
+        async with session.get(url, headers=self.headers) as response:
+            return await response.json()
+
+    async def fetch_relics(self, session, url, token, token_id):
+        payload = {'nftType': token, 'tokenId': str(token_id)}
+        async with session.post(url, json=payload) as response:
+            json_data = await response.json()
+            return json_data.get('result', {}).get('response', {}).get('relicsResponse', [])
+
+    async def process_order(self, session, token, address, order):
+        token_id = order['criteria']['data']['token']['tokenId']
+        price = order['price']['amount']['decimal']
+        exchange = order['kind']
+
+        attribute_key = "rarity" if token == "trainer" else "Rarity"
+        url_attribute = f"https://api.reservoir.tools/collections/{address}/attributes/explore/v5?tokenId={token_id}&attributeKey={attribute_key}"
+        attributes_data = await self.fetch_data(session, url_attribute)
+        attribute_rarity = attributes_data.get('attributes', [{}])[0].get('value', 'Not available')
+        attribute_floorprice = attributes_data.get('attributes', [{}])[0].get('floorAskPrices', [])
+
+        relics_data = self.relics_log.get(token_id)
+        if not relics_data:
+            url_relics = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
+            relics_data = await self.fetch_relics(session, url_relics, token, token_id)
+            self.relics_log[token_id] = relics_data
+
+        relics_value = sum(self.relic_values.get(relic.get('relicsType'), 0) * relic.get('count', 0) for relic in relics_data)
+
+        if attribute_floorprice and relics_value >= 0.15:
+            floor_price = float(attribute_floorprice[0])
+            if floor_price + relics_value >= float(price):
+                blur_link = f"https://blur.io/asset/{address}/{token_id}"
+                message = f"@everyone\n**{attribute_rarity}** Trainer: {token_id}\n{relics_data}\nFloor Price: {attribute_floorprice:.4f} ETH\nRelics Value: {relics_value:.4f} ETH\n\n**Listing Price: {price:.4f} ETH**\n{blur_link}"
+                for guild in self.bot.guilds:
+                                channels = await self.config.guild(guild).channels()
+                                for channel_id in channels:
+                                    channel = guild.get_channel(channel_id)
+                                    allowed_mentions = discord.AllowedMentions(everyone=True)
+                                    await channel.send(message, allowed_mentions=allowed_mentions)
+
+    @commands.command()
     async def snipe(self, ctx):
-        pass
-
-    @snipe.command()
-    async def setchannel(self, ctx, channel: discord.TextChannel):
-        async with self.config.guild(ctx.guild).channels() as channels:
-            if channel.id in channels:
-                await ctx.send(f"{channel.mention} is already a news feed channel.")
-                return
-            channels.append(channel.id)
-            await ctx.send(f"{channel.mention} set as a news feed channel.")
-
-    @snipe.command()
-    async def removechannel(self, ctx, channel: discord.TextChannel):
-        async with self.config.guild(ctx.guild).channels() as channels:
-            if channel.id not in channels:
-                await ctx.send(f"{channel.mention} is not a news feed channel.")
-                return
-            channels.remove(channel.id)
-            await ctx.send(f"{channel.mention} removed as a news feed channel.")
-
-    @snipe.command()
-    async def listchannels(self, ctx):
-        channels = await self.config.guild(ctx.guild).channels()
-        if not channels:
-            await ctx.send("No news feed channels set.")
-            return
-        channel_mentions = [f"<#{channel_id}>" for channel_id in channels]
-        await ctx.send(f"News feed channels: {', '.join(channel_mentions)}")
-    
-    async def fetch_data_for_addresses(self):
-        try:
-            for nfttype, address in self.nfttype.items():
-                url = self.url_reservoir.format(address=address)
-                async with self.session.get(url, headers=self.headers) as response:
-                    data = await response.json()
-                    for order in data['orders']:
-                        token_id = order['criteria']['data']['token']['tokenId']
-                        price = order['price']['amount']['decimal']
-                        exchange = order['kind']
-                        logging.info(f"Token ID: {token_id}, Exchange: {exchange}, Price: {price}")       
-        except Exception as e:
-            logging.error(f"Error occurred while fetching data from Reservoir API: {e}")
-    
-    async def fetch_trainer_data(self, token_id, nfttype):
-        relics_cache = self.relics_cache.get(token_id)
-        if relics_cache:
-            return relics_cache
-        try:
-            payload = {'nftType': nfttype, 'tokenId': str(token_id)}
-            async with self.session.post(self.url_relics, json=payload) as response:
-                data = await response.json()
-                if 'result' in data and 'response' in data['result']:
-                    relics_response = data['result']['response']['relicsResponse']
-                    relics_data = {}
-                    for relic in relics_response:
-                        relics_data[relic['relicsType']] = relic['count']
-                    self.relics_cache[token_id] = relics_data
-                    return relics_data
-        except Exception as e:
-            logging.error(f"Error occurred while fetching data from trainer API: {e}")
-        return None
-
-    def cog_unload(self):
-        if self.task:
-            self.task.cancel()
-            self.task = None
-        asyncio.create_task(self.session.close())
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for token, address in self.contract_address.items():
+                url_reservoir = f"https://api.reservoir.tools/orders/asks/v5?tokenSetId=contract%3A{address}&limit=20"
+                data = await self.fetch_data(session, url_reservoir)
+                for order in data['orders']:
+                    tasks.append(self.process_order(session, token, address, order))
+            await asyncio.gather(*tasks)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import requests
-import json
+import asyncio
+import aiohttp
 
 contract_address = {
     "trainer": "0x8a3749936e723325c6b645a0901470cd9e790b94",
@@ -12,79 +12,61 @@ headers = {
 }
 
 relic_values = {'diamond': 0.15, 'gold': 0.045, 'silver': 0.018, 'bronze': 0.009, 'wood': 0.0024, 'unRevealed': 0.0024}
+relics_log = {}
 
-# Load existing log if available
-try:
-    with open("relics_log.json", "r") as log_file:
-        relics_log = json.load(log_file)
-except FileNotFoundError:
-    relics_log = {}
+async def fetch_data(session, url):
+    async with session.get(url, headers=headers) as response:
+        return await response.json()
 
-for token, address in contract_address.items():
-    url = f"https://api.reservoir.tools/orders/asks/v5?tokenSetId=contract%3A{address}&limit=2"
-    response = requests.get(url, headers=headers)
-    data = response.json()
+async def fetch_relics(session, url, token, token_id):
+    payload = {'nftType': token, 'tokenId': str(token_id)}
+    async with session.post(url, json=payload) as response:
+        json_data = await response.json()
+        return json_data.get('result', {}).get('response', {}).get('relicsResponse', [])
 
-    print("Token:", token)
-    for order in data['orders']:
-        token_id = order['criteria']['data']['token']['tokenId']
-        price = order['price']['amount']['decimal']
-        exchange = order['kind']
-        print("Token ID:", token_id)
-        print("Price:", price)
-        print("Exchange:", exchange)
+async def process_order(session, token, address, order):
+    token_id = order['criteria']['data']['token']['tokenId']
+    price = order['price']['amount']['decimal']
+    exchange = order['kind']
 
-        # Fetch url_attribute API
-        if token == "trainer":
-            attribute_key = "rarity"
-        elif token == "pixelmon":
-            attribute_key = "Rarity"
-        url_attribute = f"https://api.reservoir.tools/collections/{address}/attributes/explore/v5?tokenId={token_id}&attributeKey={attribute_key}"
-        response_attribute = requests.get(url_attribute, headers=headers)
-        attributes_data = response_attribute.json().get('attributes', [])
-        if attributes_data:
-            attribute_rarity = attributes_data[0]['value']
-            attribute_floorprice = attributes_data[0].get('floorAskPrices', [])
+    attribute_key = "rarity" if token == "trainer" else "Rarity"
+    url_attribute = f"https://api.reservoir.tools/collections/{address}/attributes/explore/v5?tokenId={token_id}&attributeKey={attribute_key}"
+    attributes_data = await fetch_data(session, url_attribute)
+    attribute_rarity = attributes_data.get('attributes', [{}])[0].get('value', 'Not available')
+    attribute_floorprice = attributes_data.get('attributes', [{}])[0].get('floorAskPrices', [])
+
+    relics_data = relics_log.get(token_id)
+    if not relics_data:
+        url_relics = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
+        relics_data = await fetch_relics(session, url_relics, token, token_id)
+        relics_log[token_id] = relics_data
+
+    relics_value = sum(relic_values.get(relic.get('relicsType'), 0) * relic.get('count', 0) for relic in relics_data)
+
+    if attribute_floorprice and relics_value >= 0.15:
+        floor_price = float(attribute_floorprice[0])
+        if floor_price + relics_value >= float(price):
+            print("Token:", token)
+            print("Token ID:", token_id)
+            print("Price:", price)
+            print("Exchange:", exchange)
             print("Rarity:", attribute_rarity)
             print("Floor Price:", attribute_floorprice)
-        else:
-            print("Attribute Value: Not available")
+            print("Relics Count:", relics_data)
+            print("Total Relics Value:", relics_value)
+            print("Great Deal!!!")
+            print("-------------------------")
 
-        # Check if relics data is in the log
-        if token_id in relics_log:
-            relics_data = relics_log[token_id]
-            print("Relics Count (From Log):")
-        else:
-            # Fetch url_relics API based on the token type
-            if token == "trainer":
-                url_relics = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
-            elif token == "pixelmon":
-                url_relics = 'https://api-cp.pixelmon.ai/nft/get-relics-count'
-            payload = {'nftType': token, 'tokenId': str(token_id)}
-            response_relics = requests.post(url_relics, json=payload)
-            relics_data = response_relics.json().get('result', {}).get('response', {}).get('relicsResponse', [])
-            # Update the log with new data
-            relics_log[token_id] = relics_data
-            print("Relics Count (Fetched from API):")
+async def main():
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for token, address in contract_address.items():
+            url_reservoir = f"https://api.reservoir.tools/orders/asks/v5?tokenSetId=contract%3A{address}&limit=20"
+            data = await fetch_data(session, url_reservoir)
+            for order in data['orders']:
+                tasks.append(process_order(session, token, address, order))
+        await asyncio.gather(*tasks)
 
-        relics_value = 0
-        for relic in relics_data:
-            relic_type = relic['relicsType']
-            relic_count = relic['count']
-            relic_value = relic_values.get(relic_type, 0) * relic_count
-            relics_value += relic_value
-            if relic_value >= 0.15:
-                print(f"{relic_type}: {relic_count} (Value: {relic_value})")
-        print("Total Relics Value:", relics_value)
 
-        # Check if attribute_floorprice + relics_value <= price
-        if attribute_floorprice and len(attribute_floorprice) > 0 and relics_value >= 0.15:
-            floor_price = float(attribute_floorprice[0])
-            if floor_price + relics_value >= float(price):
-                print("Floor price + Relics value is less than or equal to the price!")
-
-        print("-------------------------")
-
-# Write the updated log to the file
-with open("relics_log.json", "w") as log_file:
-    json.dump(relics_log, log_file, indent=4)
+if __name__ == "__main__":
+    asyncio.run(main())
