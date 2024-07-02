a868d503dee3bea84956256dcaaefc665ae14a65
Saim <thegalbatronix@gmail.com>
2024-07-02 14:17:10 +0200

    test 3

--- arb/arb.py
+++ arb/arb.py
@@ -8,7 +8,7 @@ class Arb(commands.Cog):
         self.contract_addresses = {
             "trainer": "0x8a3749936e723325c6b645a0901470cd9e790b94",
             "pixelmon": "0x32973908faee0bf825a343000fe412ebe56f802a",
-            "pixelmon-e2": "0x32973908faee0bf825a343000fe412ebe56f802a"
+            "e2": "0x32973908faee0bf825a343000fe412ebe56f802a"  # Add the e2 contract address here
         }
         self.reservoir_api_key = "60bf8680-7718-50eb-9340-39d85f05cf7d"
         self.coinmarketcap_api_key = "a4a8acd9-732c-4857-8d86-5b1e620572b9"
@@ -30,7 +30,7 @@ class Arb(commands.Cog):
             "Uncommon": 1992,
             "Common": 1811
         }
-        self.mon_locked_amounts_pixelmon_e2 = {
+        self.mon_locked_amounts_e2 = {
             "Common": 10870,
             "Uncommon": 11957,
             "Rare": 13044,
@@ -38,51 +38,80 @@ class Arb(commands.Cog):
             "Legendary": 32610,
             "Mythical": 163054
         }
+        self.species_rarity_map = {
+            "Antonne": "Common",
+            "Arachneae": "Common",
+            "Bloopius": "Common",
+            "Boodecept": "Common",
+            "Bruizton": "Common",
+            "Bugglebug": "Common",
+            "Bullmore": "Common",
+            "Cheblammo": "Common",
+            "Cubixear": "Common",
+            "Drootle": "Common",
+            "Fowlian": "Common",
+            "Haptorid": "Common",
+            "Hazerduz": "Common",
+            "Bellowhound": "Common",
+            "Piu-2": "Common",
+            "Shelkjet": "Common",
+            "Spoutcrest": "Common",
+            "Tookem": "Common",
+            "Flowuff": "Common",
+            "Xanshadow": "Common",
+            "Brawlot": "Uncommon",
+            "Cactwodom": "Uncommon",
+            "Flickhorn": "Uncommon",
+            "Foxetacean": "Uncommon",
+            "Metascan": "Uncommon",
+            "Molebore": "Uncommon",
+            "Nethratine": "Uncommon",
+            "Sharpearl": "Uncommon",
+            "Uiop": "Uncommon",
+            "Lilamoth": "Uncommon",
+            "Sharmendu": "Uncommon",
+            "Sheedlax": "Uncommon",
+            "Wardenon": "Uncommon",
+            "Aquahond": "Rare",
+            "Apamon": "Rare",
+            "Fiveticles": "Rare",
+            "Goloidon": "Rare",
+            "Embercowl": "Rare",
+            "Hubb-L/C": "Rare",
+            "Inferknight": "Rare",
+            "Lunaful": "Rare",
+            "Octopodus": "Rare",
+            "Pinmeleon": "Rare",
+            "R7n3-B": "Rare",
+            "Rahvager": "Rare",
+            "Bergum": "Epic",
+            "Borgol": "Epic",
+            "Floratopsian": "Epic",
+            "Harrow": "Epic",
+            "Cephaqueen": "Epic",
+            "Krawarrior": "Epic",
+            "Marrohowl": "Epic",
+            "Mr.mask": "Epic",
+            "Purralycat": "Epic",
+            "Slythiss": "Epic",
+            "Repteg": "Legendary",
+            "Bengusi": "Legendary",
+            "Ardentrus": "Legendary",
+            "Grailshield": "Legendary",
+            "T.r.b.y.n": "Legendary",
+            "Primeonimbus": "Legendary",
+            "Serdrake": "Legendary",
+            "Xyvonna": "Legendary",
+            "Titanoth": "Mythical",
+            "Aonithan": "Mythical",
+            "Hariken": "Mythical",
+            "Ferrunos": "Mythical"
+        }
         self.mon_price_usd = self.get_crypto_price("MON")
         self.eth_price_usd = self.get_crypto_price("ETH")
 
-        self.commonSpecies = [
-            "Antonne", "Arachneae", "Bloopius", "Boodecept", "Bruizton",
-            "Bugglebug", "Bullmore", "Cheblammo", "Cubixear", "Drootle",
-            "Fowlian", "Haptorid", "Hazerduz", "Bellowhound", "Piu-2",
-            "Shelkjet", "Spoutcrest", "Tookem", "Flowuff", "Xanshadow"
-        ]
-
-        self.uncommonSpecies = [
-            "Brawlot", "Cactwodom", "Flickhorn", "Foxetacean", "Metascan",
-            "Molebore", "Nethratine", "Sharpearl", "Uiop", "Lilamoth",
-            "Sharmendu", "Sheedlax", "Wardenon"
-        ]
-
-        self.rareSpecies = [
-            "Aquahond", "Apamon", "Fiveticles", "Goloidon", "Embercowl",
-            "Hubb-L/C", "Inferknight", "Lunaful", "Octopodus", "Pinmeleon",
-            "R7n3-B", "Rahvager"
-        ]
-
-        self.epicSpecies = [
-            "Bergum", "Borgol", "Floratopsian", "Harrow", "Cephaqueen",
-            "Krawarrior", "Marrohowl", "Mr.mask", "Purralycat", "Slythiss"
-        ]
-
-        self.legendarySpecies = [
-            "Repteg", "Bengusi", "Ardentrus", "Grailshield", "T.r.b.y.n",
-            "Primeonimbus", "Serdrake", "Xyvonna"
-        ]
-
-        self.mythicalSpecies = [
-            "Titanoth", "Aonithan", "Hariken", "Ferrunos"
-        ]
-
     def fetch_nft_data(self, contract_name, contract_address):
-        if contract_name == 'trainer':
-            attribute_key = 'rarity'
-        elif contract_name == 'pixelmon-e2':
-            attribute_key = 'Species'
-        else:
-            attribute_key = 'Rarity'
-        
-        url = f"https://api.reservoir.tools/collections/{contract_address}/attributes/explore/v5?attributeKey={attribute_key}"
+        url = f"https://api.reservoir.tools/collections/{contract_address}/attributes/explore/v5?attributeKey={'rarity' if contract_name == 'trainer' else 'Rarity'}"
         headers = {
             "accept": "*/*",
             "x-api-key": self.reservoir_api_key
@@ -104,43 +133,38 @@ class Arb(commands.Cog):
         data = response.json()
         return data["data"][symbol]["quote"]["USD"]["price"]
 
-    def calculate_lowest_floor_prices(self, species_data):
-        lowest_prices = {
-            'Common': float('inf'),
-            'Uncommon': float('inf'),
-            'Rare': float('inf'),
-            'Epic': float('inf'),
-            'Legendary': float('inf'),
-            'Mythical': float('inf')
+    def fetch_e2_floor_prices(self):
+        url = "https://api.reservoir.tools/collections/0x32973908faee0bf825a343000fe412ebe56f802a/attributes/explore/v5?attributeKey=Species&limit=200"
+        headers = {
+            "accept": "*/*",
+            "x-api-key": self.reservoir_api_key
+        }
+        response = requests.get(url, headers=headers)
+        nft_data = response.json()
+
+        e2_lowest_prices = {
+            "Common": float('inf'),
+            "Uncommon": float('inf'),
+            "Rare": float('inf'),
+            "Epic": float('inf'),
+            "Legendary": float('inf'),
+            "Mythical": float('inf')
         }
 
-        if not species_data:
-            return lowest_prices
+        for attribute in nft_data['attributes']:
+            species = attribute['value']
+            rarity = self.species_rarity_map.get(species)
 
-        for species in species_data['attributes']:
-            if not isinstance(species, dict):
+            if rarity == 'Unknown' or rarity is None:
                 continue
 
-            species_name = species.get('value', {}).get('Species', '')
-            floor_ask_price = species.get('value', {}).get('FloorAskPrice', 0)
+            floor_ask_prices = attribute['floorAskPrices']
+            if rarity in e2_lowest_prices:
+                min_price = min(floor_ask_prices)
+                if min_price < e2_lowest_prices[rarity]:
+                    e2_lowest_prices[rarity] = min_price
 
-            # Rest of your logic
-
-
-            if species_name in self.commonSpecies:
-                lowest_prices['Common'] = min(lowest_prices['Common'], floor_ask_price)
-            elif species_name in self.uncommonSpecies:
-                lowest_prices['Uncommon'] = min(lowest_prices['Uncommon'], floor_ask_price)
-            elif species_name in self.rareSpecies:
-                lowest_prices['Rare'] = min(lowest_prices['Rare'], floor_ask_price)
-            elif species_name in self.epicSpecies:
-                lowest_prices['Epic'] = min(lowest_prices['Epic'], floor_ask_price)
-            elif species_name in self.legendarySpecies:
-                lowest_prices['Legendary'] = min(lowest_prices['Legendary'], floor_ask_price)
-            elif species_name in self.mythicalSpecies:
-                lowest_prices['Mythical'] = min(lowest_prices['Mythical'], floor_ask_price)
-
-        return lowest_prices
+        return e2_lowest_prices
 
     async def fetch_and_print_data(self, ctx, contract_name):
         contract_address = self.contract_addresses.get(contract_name)
@@ -156,19 +180,29 @@ class Arb(commands.Cog):
         discounts_usd = []
         discounts_pct = []
 
-        if contract_name == "trainer":
-            mon_locked_amounts = self.mon_locked_amounts_trainers
-        elif contract_name == "pixelmon":
-            mon_locked_amounts = self.mon_locked_amounts_pixelmon
-        elif contract_name == "pixelmon-e2":
-            mon_locked_amounts = self.mon_locked_amounts_pixelmon_e2
-        else:
-            mon_locked_amounts = {}
+        mon_locked_amounts = (
+            self.mon_locked_amounts_trainers if contract_name == "trainer"
+            else self.mon_locked_amounts_pixelmon if contract_name == "pixelmon"
+            else self.mon_locked_amounts_e2 if contract_name == "e2"
+            else {}
+        )
 
-        lowest_floor_prices = self.calculate_lowest_floor_prices(nft_data)
+        if contract_name == "e2":
+            # Fetch lowest floorAskPrices from e2 API
+            e2_prices = self.fetch_e2_floor_prices()
 
-        for rarity in lowest_floor_prices:
-            floor_ask_price_usd = lowest_floor_prices[rarity]
+        for attribute in nft_data['attributes']:
+            rarity = attribute['value']
+
+            if rarity == 'Unknown':
+                continue
+
+            floor_ask_price_eth = e2_prices.get(rarity, 0) if contract_name == "e2" else attribute['floorAskPrices'][0]
+            floor_ask_price_usd = floor_ask_price_eth * self.eth_price_usd
+
+            if floor_ask_price_usd == 0.00:
+                continue
+
             mon_locked_amount = mon_locked_amounts.get(rarity, 0)
             mon_bought_with_floor_price = floor_ask_price_usd / self.mon_price_usd
             discount_usd = mon_locked_amount - mon_bought_with_floor_price
@@ -180,26 +214,29 @@ class Arb(commands.Cog):
             discounts_usd.append(discount_usd)
             discounts_pct.append(discount_pct)
 
-        table_data = []
-        for i in range(len(rarities)):
-            table_data.append([rarities[i], mon_locked_usd[i], mon_value_usd_list[i], discounts_usd[i], discounts_pct[i]])
+        table_data = {
+            "Rarity": rarities,
+            "MON_locked ($)": mon_locked_usd,
+            "MON_bought ($)": mon_value_usd_list,
+            "Discount ($)": discounts_usd,
+            "Discount (%)": discounts_pct
+        }
 
-        output_table = tabulate(table_data, headers=["Rarity", "Mon Locked", "Mon Equivalent", "Discount (MON)", "Discount (%)"])
-        await ctx.send(f"Contract: {contract_name} ({contract_address})\n```\n{output_table}\n```")
+        table = tabulate(table_data, headers="keys", tablefmt="fancy_grid")
 
-    @commands.group()
-    async def arb(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.send_help(ctx.command)
+        await ctx.send(f"```{table}```")
 
-    @arb.command()
-    async def trainer(self, ctx: int):
-        await self.fetch_and_print_data(ctx, 'trainer')
+    @commands.command()
+    async def pixelmon(self, ctx):
+        await self.fetch_and_print_data(ctx, "pixelmon")
 
-    @arb.command()
-    async def pixelmon(self, ctx: int):
-        await self.fetch_and_print_data(ctx, 'pixelmon')
+    @commands.command()
+    async def trainer(self, ctx):
+        await self.fetch_and_print_data(ctx, "trainer")
 
-    @arb.command()
-    async def pixelmon_e2(self, ctx: int):
-        await self.fetch_and_print_data(ctx, 'pixelmon-e2')
+    @commands.command()
+    async def e2(self, ctx):
+        await self.fetch_and_print_data(ctx, "e2")
+
+def setup(bot):
+    bot.add_cog(Arb(bot))
